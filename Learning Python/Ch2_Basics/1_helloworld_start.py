b3219d74554bb1cdd9ac4afa93b168b6583a0d2e
Sidrat Habib <70166676+sidrathabib@users.noreply.github.com>
2024-01-11 20:38:44 -0500

    Delete Learning Python directory

--- Learning Python/Ch2_Basics/1_helloworld_start.py
+++
@@ -1,9 +0,0 @@
-
-
-def main(): #defining a function 
-    print("Hello World!") #python uses indent to check where the code belongs
-    name = input("What is your name? ") #this vode belongs to the main function 
-    print("Nice to meet you ", name)
-
-if __name__ == "__main__":
-    main() #checks to see if this file is loaded, executed as main program 
